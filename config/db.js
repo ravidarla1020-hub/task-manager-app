a4df54db0721eebe51bd7a64e64187472b2e1d39
Darla Ravi Kiran <ravidarla1020@gmail.com>
2026-08-05 01:59:16 +0530

    Add SSL config for TiDB Cloud connection

--- config/db.js
+++ config/db.js
@@ -3,12 +3,16 @@ require('dotenv').config();
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 4000,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = db;
